3d5ad383bc7789002d03a300cb3cee65ff286eb4
陈帅帅 <1513323081@qq.com>
2019-01-25 14:36:52 +0800

    chore(webpack): add webpack4

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin'); //每次构建清理dist目录
+module.exports = {
+	mode:'production',
+	entry: './src/index.js',
+	output: {
+		filename: 'plus.min.js',
+		library: 'plus',
+		libraryTarget: 'umd',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader'
+			}
+		}]
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist'])
+	]
+};
